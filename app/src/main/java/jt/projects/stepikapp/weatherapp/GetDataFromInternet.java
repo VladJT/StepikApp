807cbcba4666a5d75af326c9184ab3a24e0099f2
Богданков Владислав <onlyrus@hotmail.com>
2022-04-14 21:32:34 +0300

    my

---
+++ app/src/main/java/jt/projects/stepikapp/weatherapp/GetDataFromInternet.java
@@ -0,0 +1,68 @@
+package jt.projects.stepikapp.weatherapp;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class GetDataFromInternet extends AsyncTask<URL, Void, String> {
+
+    private static final String TAG = "GetDataFromInternet";
+
+    public GetDataFromInternet(AsyncResponce delegate) {
+        this.delegate = delegate;
+    }
+
+    public interface AsyncResponce{
+        void processFinish(String response);
+    }
+    public AsyncResponce delegate;
+
+    // для GUI
+    @Override
+    protected void onPreExecute() {
+        Log.d(TAG, "onPreExecute: ");
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(URL[] urls) {
+        Log.d(TAG, "doInBackground: ");
+        String result = "";
+        try {
+            result = getDataFromInternet(urls[0]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Log.d(TAG, "onPostExecute: " + result);
+        delegate.processFinish(result);
+    }
+
+    String getDataFromInternet(URL url) throws IOException {
+        String result = "";
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = conn.getInputStream();
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");// конец строки
+            boolean hasData = scanner.hasNext();
+            if (hasData) {
+                result = scanner.next();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            conn.disconnect();
+            return result;
+        }
+    }
+}
---
+++ app/src/main/java/jt/projects/stepikapp/weatherapp/WeatherMainActivity.java
@@ -0,0 +1,93 @@
+package jt.projects.stepikapp.weatherapp;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import jt.projects.stepikapp.R;
+
+public class WeatherMainActivity extends AppCompatActivity implements GetDataFromInternet.AsyncResponce, View.OnClickListener {
+
+    private static final String TAG = "WeatherMainActivity";
+    private Button buttonSearch;
+    private EditText editTextCityName;
+    private TextView textviewCityName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+        buttonSearch = findViewById(R.id.buttonSearch);
+        buttonSearch.setOnClickListener(this);
+        editTextCityName = findViewById(R.id.editTextCityName);
+        textviewCityName = findViewById(R.id.textCity);
+
+    }
+
+    @Override
+    public void processFinish(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONObject weather = jsonObject.getJSONObject("main");
+            JSONObject sys = jsonObject.getJSONObject("sys");
+
+            TextView temp = findViewById(R.id.textTemperature);
+            String tempInKalvin = weather.getString("temp");
+            float tempC = Float.valueOf(tempInKalvin) - 273.15f;
+            temp.setText(tempC + "");
+
+            TextView preasure = findViewById(R.id.textPreasure);
+            preasure.setText(weather.getString("pressure"));
+
+            TextView sunrise = findViewById(R.id.textTimeSunrise);
+            Locale m = new Locale("ru", "RU");
+            SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss", m);
+            String dateSunRise = formatter.format(new Date(Long.parseLong(sys.getString("sunrise")) * 1000 + (60 * 60 * 1000) * 3));
+            sunrise.setText(dateSunRise);
+
+            TextView sunset = findViewById(R.id.textTimeSunset);
+            String dateSunset = formatter.format(new Date(Long.parseLong(sys.getString("sunset")) * 1000 + (60 * 60 * 1000) * 3));
+            sunset.setText(dateSunset);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Log.d(TAG, "processFinish: " + response);
+    }
+
+    @Override
+    public void onClick(View v) {
+        URL url = buildUrl(editTextCityName.getText().toString());
+        textviewCityName.setText(editTextCityName.getText().toString());
+        new GetDataFromInternet(this).execute(url);
+    }
+
+    private URL buildUrl(String city) {
+        String Base = "https://api.openweathermap.org/data/2.5/weather";
+        String PARAM_CITY = "q";
+        String PARAM_APP_ID = "appid";
+        String appidValue = "2e7129709316d930ca03fb6be2f03ed3";
+        Uri myUri = Uri.parse(Base).buildUpon().appendQueryParameter(PARAM_CITY, city).appendQueryParameter(PARAM_APP_ID, appidValue).build();
+        URL myUrl = null;
+        try {
+            myUrl = new URL(myUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return myUrl;
+    }
+}
